fe0947569579776b2c268c86c7cd1444b6c0242e
Kiril Bourakov <kirbou06012@gmail.com>
2024-12-09 13:12:02 -0400

    replaced .gd with c#

---
+++ Scenes/ground.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class ground : StaticBody2D
+{
+	private Node2D _ground;
+    private StaticBody2D _staticBody2D;
+
+	public override void _Ready()
+	{
+		_ground = GetParent<Node2D>();
+        _staticBody2D = this;
+	}
+
+	public void add(){
+		StaticBody2D copyNode = (StaticBody2D)_staticBody2D.Duplicate();
+		copyNode.Position = new Vector2(_staticBody2D.Position.X + 330, _staticBody2D.Position.Y);
+
+		_ground.AddChild(copyNode);
+	}
+
+	public void Remove()
+    {
+        _staticBody2D.QueueFree();
+    }
+}

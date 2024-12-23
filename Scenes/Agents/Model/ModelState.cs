2136f2b87ce1a849bba5f1ec08efb7e5a0c6fc83
Kiril B <kirbou06012@gmail.com>
2024-12-23 18:53:40 -0400

    improved Reproduce method with elitism and GaussianMutations

--- Scenes/Agents/Model/ModelState.cs
+++ Scenes/Agents/Model/ModelState.cs
@@ -8,6 +8,7 @@ public partial class ModelState : Node
 	public ModelPlayer[] models {get;set;}
 	private int modelsCount;
 	public bool internalManaged = false;
+	private Random random = new Random();
 
 	public ModelState(){
 		this.models = new ModelPlayer[GEN_SIZE];
@@ -26,42 +27,66 @@ public partial class ModelState : Node
 
 	public void Reproduce(){
 		int totalFitness = 0;
-		Random random = new Random();
 
+		// generate the reproduction wheel
 		foreach (var model in this.models)
 		{
 			totalFitness += model.GetPoints()+1;
 		}
-
 		ModelPlayer[] wheel = new ModelPlayer[totalFitness];
-
 		int index = 0;
 		foreach (var model in this.models)
 		{
-			for (int i = 0; i < model.GetPoints(); i++)
+			for (int j = 0; j < model.GetPoints(); j++)
 			{
 				wheel[index] = model;
 				index++;
 			}
 		}
 
+		//reproduce
 		ModelPlayer[] newGeneration = new ModelPlayer[GEN_SIZE];
-		for (int i = 0; i < GEN_SIZE; i++){
+
+
+		// elitism; keep top 5% of models that have more then 1 point
+		int elitismSize = (int)Math.Floor(0.05 * GEN_SIZE);
+		Array.Sort(models, (x, y) => y.GetPoints().CompareTo(x.GetPoints()));
+		for (int i = 0; i < elitismSize; i++){
+			if (models[i].GetPoints() > 1){
+				newGeneration[i] = models[i];
+				i++;
+			} 
+			else {
+				break;
+			}
+		}
+
+		for (int i = elitismSize; i < GEN_SIZE; i++){
 			// get the two parents
-			ModelPlayer mate1 = this.models[i];
+			ModelPlayer mate1 = this.models[random.Next(0, totalFitness)];
 			ModelPlayer mate2 = wheel[random.Next(0, totalFitness)];
+			while (mate1 == mate2){
+				mate2 = wheel[random.Next(0, totalFitness)];
+			}
 
 			ModelPlayer child = new ModelPlayer();
-			for (int j = 0; j <= child.weights.Length; j++){
+			for (int j = 0; j < child.weights.Length; j++){
 				// choose a random parent gene
 				child.weights[j] = random.Next(0, 2) == 1 ? mate2.weights[j] : mate1.weights[j];
 
 				//apply a mutation between +1 and -1
-				child.weights[j] += (random.NextDouble()*2)-1;
+				child.weights[j] += GetGaussianMutation();
 			}
 			newGeneration[i] = child;
+			i++;
 		}
 		this.models = newGeneration;
+	}
+
+	public double GetGaussianMutation(){
+		double u1 = random.NextDouble();
+		double u2 = random.NextDouble();
+		return 0.1 * Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2);
 	}
 
 

4296ede30b3c257d48170d7084f783f9ee83c66d
Kiril B <kirbou06012@gmail.com>
2024-12-27 21:07:47 -0400

    revised pipespawning to be more inline with flappy bird

--- Scenes/World/pipe/PipeSpawner.cs
+++ Scenes/World/pipe/PipeSpawner.cs
@@ -8,15 +8,13 @@ public partial class PipeSpawner : Node2D
 	private PackedScene pointScene;
 	private Random random;
 
-	private const int CENTER_MIN = -150;
-	private const int CENTER_MAX = -20;
-	private const int LAST_GAP_MIN = 50;
-	private const int LAST_GAP_MAX = 100;
+	private const int CENTER_MIN = -120;
+	private const int CENTER_MAX = -40;
+	private const int GAP = 50;
 
 	// Called when the node enters the scene tree for the first time.
 
-	private int lastCenter;
-	private int lastGap;
+	private int center;
 	private Vector2 SpawnPos;
 	public override void _Ready()
 	{
@@ -28,23 +26,25 @@ public partial class PipeSpawner : Node2D
 		this.SpawnPos = new Vector2(0,0);
 		this.random = new Random();
 
-		this.lastCenter = this.random.Next(CENTER_MIN, CENTER_MAX);
-		this.lastGap = this.random.Next(LAST_GAP_MIN, LAST_GAP_MAX);
+		this.center = this.random.Next(CENTER_MIN, CENTER_MAX);
 	}
 
 	public void SpawnPipes(){
 		float x = this.SpawnPos.X+100;
-		int center = Math.Max(CENTER_MIN, this.lastCenter + this.random.Next(-25, +25));
-		center = Math.Min(CENTER_MAX, center);
-		int gap = Math.Max(LAST_GAP_MIN, this.lastGap + this.random.Next(-25, +25));
-		gap = Math.Min(LAST_GAP_MAX, gap);
+
+		int newCenter = this.center + this.random.Next(-40, 40);
+		while (newCenter > CENTER_MAX || newCenter < CENTER_MIN){
+			GD.Print("looking for new center");
+			newCenter = this.center + this.random.Next(-40, 40);
+		}
+		this.center = newCenter;
 		
 		Pipe top = (Pipe)this.pipeScene.Instantiate();
-		top.Position = new Vector2(x, center-(gap/2)-75);
+		top.Position = new Vector2(x, center-(GAP/2)-75);
 		top.RotationDegrees += 180;
 
 		Pipe bottom = (Pipe)this.pipeScene.Instantiate();
-		bottom.Position = new Vector2(x, center+(gap/2)+75);
+		bottom.Position = new Vector2(x, center+(GAP/2)+75);
 
 		PointGiver pointGiver = (PointGiver)this.pointScene.Instantiate();
 		pointGiver.Position = new Vector2(x, 0);
@@ -63,7 +63,6 @@ public partial class PipeSpawner : Node2D
 
 	public void SpawnTimerDone(){
 		SpawnPipes();
-		timer.WaitTime = this.random.NextDouble()+0.25;
 		this.timer.Start();
 	}
 

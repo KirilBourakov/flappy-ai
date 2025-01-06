8d20c76e8ff598b210a6859a78a3f5f12caf0c7f
Kiril B <kirbou06012@gmail.com>
2025-01-05 21:02:40 -0400

    added Species Tests

---
+++ test/SpeciesTest.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using GdUnit4;
+using Godot;
+using NEAT;
+
+[TestSuite]
+public class SpeciesTest 
+{
+    public Species genSpecies(){
+        GenePool pool = new();
+        List<NeuralNetwork> members = new List<NeuralNetwork>{
+            new NeuralNetwork(pool, true, new List<ConnectGene>()),
+            new NeuralNetwork(pool, true, new List<ConnectGene>()),
+            new NeuralNetwork(pool, true, new List<ConnectGene>())
+        };
+        members[0].fitness = 2;
+        members[1].fitness = 3;
+        members[2].fitness = 4;
+        Species species = new(members);
+        return species;
+    }
+
+    [TestCase]
+    public void updateAvgFitnessTest(){
+        Species species = genSpecies();
+        double avg = species.updateAvgFitness();
+
+        Assertions.AssertThat(avg).Equals(3d);
+        Assertions.AssertThat(species.avgFitness).Equals(3d);
+    }
+
+    [TestCase]
+    public void CreateNewGenerationCorrectLengthTest(){
+        Species species = genSpecies();
+        species.updateAvgFitness();
+
+        Assertions.AssertInt(species.CreateNewGeneration(species.avgFitness).Count).IsEqual(species.memebers.Count);
+        Assertions.AssertInt(species.CreateNewGeneration(species.avgFitness*2).Count).IsEqual(species.memebers.Count/2);
+        Assertions.AssertInt(species.CreateNewGeneration(species.avgFitness/2).Count).IsEqual(species.memebers.Count*2);
+    }
+}
